5c08b065bb5405c7dd832d92008d17390546792a
Nahid <nahidshaiket10300@gmail.com>
2017-04-25 01:27:04 +0600

    added the sqlite3

--- userinformation/views.py
+++ userinformation/views.py
@@ -13,57 +13,58 @@ import datetime
 # Create your views here.
 @api_view(['POST'])
 def login(request):
-    print(request.body)
-    try:
-        result = json.loads(str(request.body, 'utf-8'))
-    except Exception as e:
-        return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
-    # print(str(result))
-    try:
-        user = User.objects.get(username=result['user_id'])
-        print("Hello")
-        try:
-            token = Token.objects.create(user=user)
-        except:
-            delete_token(user)
-            token = Token.objects.create(user=user)
-        user_info = UserInformations.objects.get(user=user)
-        user_info.access_key = result['access_key']
-        user_info.save()
-
-        user_info_serializer = UserInfoSerializer(user_info)
-        user_serializer = UserSerializer(user)
-        return Response({"token": token.key, "user": user_serializer.data, "user_info": user_info_serializer.data},
-                        status=status.HTTP_200_OK)
-    except Exception as e:
-        try:
-            print(e)
-            username = result['user_id']
-            email = result['email']
-            password = '123456'
-            user = User.objects.create_user(username, email, password)
-            name = result['name'].split(" ")
-            user.first_name = name[0]
-            if(name[1]):
-                user.last_name = name[1]
-            user.is_active = True
-            user.save()
-            user_info = UserInformations.objects.create(name=result['name'],
-                                                        email=result['email'],
-                                                        user=user,
-                                                        created_at=datetime.datetime.now(),
-                                                        updated_at=datetime.datetime.now(),
-                                                        access_key=result['access_key'])
-            user_info_serializer = UserInfoSerializer(user_info)
-            user_serializer = UserSerializer(user)
-            try:
-                token = Token.objects.create(user=user)
-            except Exception as e:
-                print(e)
-            return Response({"token": token.key, "user": user_serializer.data, "user_info": user_info_serializer.data}, status=status.HTTP_200_OK)
-        except Exception as e:
-            print(e)
-            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+    print(request.POST['email'])
+    # print(request.body)
+    # try:
+    #     result = json.loads(str(request.body, 'utf-8'))
+    # except Exception as e:
+    #     return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+    # # print(str(result))
+    # try:
+    #     user = User.objects.get(username=result['user_id'])
+    #     print("Hello")
+    #     try:
+    #         token = Token.objects.create(user=user)
+    #     except:
+    #         delete_token(user)
+    #         token = Token.objects.create(user=user)
+    #     user_info = UserInformations.objects.get(user=user)
+    #     user_info.access_key = result['access_key']
+    #     user_info.save()
+    #
+    #     user_info_serializer = UserInfoSerializer(user_info)
+    #     user_serializer = UserSerializer(user)
+    #     return Response({"token": token.key, "user": user_serializer.data, "user_info": user_info_serializer.data},
+    #                     status=status.HTTP_200_OK)
+    # except Exception as e:
+    #     try:
+    #         print(e)
+    #         username = result['user_id']
+    #         email = result['email']
+    #         password = '123456'
+    #         user = User.objects.create_user(username, email, password)
+    #         name = result['name'].split(" ")
+    #         user.first_name = name[0]
+    #         if(name[1]):
+    #             user.last_name = name[1]
+    #         user.is_active = True
+    #         user.save()
+    #         user_info = UserInformations.objects.create(name=result['name'],
+    #                                                     email=result['email'],
+    #                                                     user=user,
+    #                                                     created_at=datetime.datetime.now(),
+    #                                                     updated_at=datetime.datetime.now(),
+    #                                                     access_key=result['access_key'])
+    #         user_info_serializer = UserInfoSerializer(user_info)
+    #         user_serializer = UserSerializer(user)
+    #         try:
+    #             token = Token.objects.create(user=user)
+    #         except Exception as e:
+    #             print(e)
+    #         return Response({"token": token.key, "user": user_serializer.data, "user_info": user_info_serializer.data}, status=status.HTTP_200_OK)
+    #     except Exception as e:
+    #         print(e)
+    #         return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
 def delete_token(user):
     try:
