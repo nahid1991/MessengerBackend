486609233cd7fa967f4e0bcbb8859ecd02ca2129
Nahid <nahidshaiket10300@gmail.com>
2017-05-05 15:18:21 +0600

    friend request added

--- messaging/models.py
+++ messaging/models.py
@@ -6,3 +6,8 @@ class Messages(models.Model):
     sender = models.ForeignKey(User, null=True, related_name='sender')
     receiver = models.ForeignKey(User, null=True, related_name='receiver')
     text = models.TextField(max_length=500)
+
+class Friends(models.Model):
+    friend_one = models.ForeignKey(User, null=True, related_name='friend_one')
+    friend_two = models.ForeignKey(User, null=True, related_name='friend_two')
+    accepted = models.BooleanField(default=False)
