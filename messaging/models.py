63a38c222fbe379184f690f28bd6902603703f9d
Nahid <nahidshaiket10300@gmail.com>
2017-05-05 15:14:00 +0600

    messaging table added

--- messaging/models.py
+++ messaging/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Messages(models.Model):
+    sender = models.ForeignKey(User, null=True, related_name='sender')
+    receiver = models.ForeignKey(User, null=True, related_name='receiver')
+    text = models.TextField(max_length=500)
